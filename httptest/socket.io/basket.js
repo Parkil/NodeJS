90bf7cad5068f3519f747220117af99be5c986d0
일 <@Parkil>
2015-04-06 20:11:57 +0900

    1.장바구니 예제 업로드 2.위치 추적 예제 업로드

---
+++ httptest/socket.io/basket.js
@@ -0,0 +1,96 @@
+/**
+ * NodeJS Websocket 예제
+ */
+
+var socketio = require('c:/Dev/language/nodejs/node_modules/socket.io');
+var http 	 = require('http');
+var fs		 = require('fs');
+var logger	 = require('c:/Dev/language/nodejs/node_modules/custom-logger').config({level : 0});
+var express	 = require('c:/Dev/language/nodejs/node_modules/express');
+var ejs		 = require('c:/Dev/language/nodejs/node_modules/ejs');
+
+var counter = 0;
+
+//개별 상품 저장 구조
+function Product(name, image, price, count) {
+	this.index	= counter++;
+	this.name	= name;
+	this.price	= price;
+	this.count	= count;
+}
+
+//전체상품목록
+var Products = [
+                new Product('JavaScript', 'chrome.png', 28000,30),
+                new Product('jQuery'	, 'chrome.png', 28000,30),
+                new Product('node.js'	, 'chrome.png', 32000,30),
+                new Product('Socket.io'	, 'chrome.png', 17000,30),
+                new Product('Connect'	, 'chrome.png', 18000,30),
+                new Product('Express'	, 'chrome.png', 31000,30),
+                new Product('EJS'		, 'chrome.png', 12000,30),
+                ];
+
+var app = express();
+var server = http.createServer(app).listen(9001, function(){logger.info('Server Started port 9001');});
+
+//이미지 저장 폴더
+app.use(express.static('public',__dirname + 'public'));
+
+app.get('/', function(request,response) {
+	var html = fs.readFileSync('basket.html', 'utf-8');
+	
+	response.send(ejs.render(html, {
+		products : Products
+	}));
+});
+
+
+//websocket
+var io= socketio.listen(server);
+
+io.sockets.on('connection', function(socket) {
+	function onReturn(index) {
+		Products[index].count++;
+		
+		clearTimeout(cart[index].timerID);
+		
+		delete cart[index];
+		
+		io.sockets.emit('count', {
+			index : index,
+			count : Products[index].count
+		});
+	}
+	
+	var cart = {};
+	
+	socket.on('cart', function(index) {
+		Products[index].count--;
+		
+		cart[index] = {};
+		cart[index].index = index;
+		cart[index].timerID = setTimeout(function() {
+			onReturn(index);
+		}, 1000*60*60); //장바구니 제한시간 설정
+		
+		io.sockets.emit('count', {
+			index : index,
+			count : Products[index].count
+		});
+	});
+	
+	socket.on('buy', function(index) {
+		clearTimeout(cart[index].timerID);
+		
+		delete cart[index];
+		
+		io.sockets.emit('count', {
+			index : index,
+			count : Products[index].count
+		});
+	});
+	
+	socket.on('return', function(index) {
+		onReturn(index);
+	});
+});
--- httptest/test.js
+++ httptest/test.js
@@ -8,89 +8,57 @@ var fs		 = require('fs');
 var logger	 = require('c:/Dev/language/nodejs/node_modules/custom-logger').config({level : 0});
 var express	 = require('c:/Dev/language/nodejs/node_modules/express');
 var ejs		 = require('c:/Dev/language/nodejs/node_modules/ejs');
-
-var counter = 0;
-
-//개별 상품 저장 구조
-function Product(name, image, price, count) {
-	this.index	= counter++;
-	this.name	= name;
-	this.price	= price;
-	this.count	= count;
-}
-
-//전체상품목록
-var Products = [
-                new Product('JavaScript', 'chrome.png', 28000,30),
-                new Product('jQuery'	, 'chrome.png', 28000,30),
-                new Product('node.js'	, 'chrome.png', 32000,30),
-                new Product('Socket.io'	, 'chrome.png', 17000,30),
-                new Product('Connect'	, 'chrome.png', 18000,30),
-                new Product('Express'	, 'chrome.png', 31000,30),
-                new Product('EJS'		, 'chrome.png', 12000,30),
-                ];
+var hashmap	 = require('c:/Dev/language/nodejs/node_modules/hashmap');
 
 var app = express();
 var server = http.createServer(app).listen(9001, function(){logger.info('Server Started port 9001');});
 
+function tracker(id, name, lat, lon, date) {
+	this.id = id;
+	this.name = name;
+	this.lat = lat;
+	this.lon = lon;
+	this.date = date;
+}
+
 //이미지 저장 폴더
 app.use(express.static('public',__dirname + 'public'));
 
-app.get('/', function(request,response) {
+app.get('/tracker', function(request,response) {
 	var html = fs.readFileSync('test.html', 'utf-8');
 	
-	response.send(ejs.render(html, {
-		products : Products
-	}));
+	response.send(html);
+});
+
+app.get('/observer', function(request,response) {
+	var html = fs.readFileSync('test2.html', 'utf-8');
+	
+	response.send(html);
+});
+
+app.get('/showdata', function(request,response) {
+	response.send(hashmap);
 });
 
 
 //websocket
-var io= socketio.listen(server);
+var io = socketio.listen(server);
+var id = 0;
 
 io.sockets.on('connection', function(socket) {
-	function onReturn(index) {
-		Products[index].count++;
-		
-		clearTimeout(cart[index].timerID);
-		
-		delete cart[index];
-		
-		io.sockets.emit('count', {
-			index : index,
-			count : Products[index].count
-		});
-	}
-	
-	var cart = {};
-	
-	socket.on('cart', function(index) {
-		Products[index].count--;
-		
-		cart[index] = {};
-		cart[index].index = index;
-		cart[index].timerID = setTimeout(function() {
-			onReturn(index);
-		}, 1000*60*60); //장바구니 제한시간 설정
-		
-		io.sockets.emit('count', {
-			index : index,
-			count : Products[index].count
-		});
+	socket.on('join', function(data) {
+		socket.join(data);
 	});
 	
-	socket.on('buy', function(index) {
-		clearTimeout(cart[index].timerID);
+	socket.on('location', function(data) {
+		var track_info = new tracker(id++, data.name, data.latitude, data.longitude);
 		
-		delete cart[index];
+		hashmap.set(id, track_info);
 		
-		io.sockets.emit('count', {
-			index : index,
-			count : Products[index].count
+		io.sockets.in(data.name).emit('receive' ,{
+			latitude : data.latitude,
+			longitude : data.longitude,
+			date : Date.now()
 		});
-	});
-	
-	socket.on('return', function(index) {
-		onReturn(index);
 	});
 });
