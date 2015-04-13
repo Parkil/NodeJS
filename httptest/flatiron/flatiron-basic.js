b3fd271a5bb7ae63d4faafdc1cd8f6d4d724f41f
일 <@Parkil>
2015-04-13 20:43:07 +0900

    기존 flatiron-basic.js에 plates예제 및 html 파일을 읽어서 표시하는 예제 추가

--- httptest/flatiron/flatiron-basic.js
+++ httptest/flatiron/flatiron-basic.js
@@ -1,24 +1,80 @@
 /*
- * flatiron framework 예제(기본)
+ * flatiron framework 예제
  * 
  * flatiron.union : nodejs.http와 비슷한 역할을 하는듯
  */
 
 var http	 = require('http');
+var fs		 = require('fs');
 
-var flatiron = require('d:/Dev/language/nodejs/node_modules/flatiron');
+var flatiron = require('c:/Dev/language/nodejs/node_modules/flatiron');
 var app		 = flatiron.app;
 
-var logger	 = require('d:/Dev/language/nodejs/node_modules/custom-logger').config({ level : 0 });
+var logger	 = require('c:/Dev/language/nodejs/node_modules/custom-logger').config({ level : 0 });
 
-var director = require('d:/Dev/language/nodejs/node_modules/director');
+var director = require('c:/Dev/language/nodejs/node_modules/director');
+var plates	 = require('c:/Dev/language/nodejs/node_modules/plates');
 
 app.use(flatiron.plugins.http);
 
+/*
+ * flatiron plates
+ * 
+ * [html의 내용을 변경]
+ * -html tag안의 innerHTML 변경
+ * var html   = '<div id="zzz"></div>';
+ * var data	  = {'zzz' : 'div value'};
+ * var result = plates.bind(html, data);
+ * 
+ * div tag안의 text가 'div value'로 변경된다.
+ * 
+ * -html tag의 속성 변경
+ * var html  = '<a href="/"></a>';
+ * var data2 = {'newurl' : 'http://www.google.co.kr'};
+ * var map	 = plates.Map();
+ * 
+ * map.where('href').is('/').insert('newurl');
+ * 
+ * var result = plates.bind(html, data2, map);
+ * 
+ * a href='/' 의 내용이 http://www.google.co.kr로 변경된다.
+ */
+var html	= '<div id="zzz"><a href="/zzz/ttt"></a></div>';
+var html2	= '<div class="name"></div>';
+
+var collection = [
+                  	{'name' : 'aaa'},
+                  	{'name' : 'bbb'},
+                  	{'name' : 'ccc'}
+                  ];
+	
+var data = {'newurl' : 'http://www.google.co.kr'};
+var map	 = plates.Map();
+
+//map.where('zzz').where('href').is('/').insert('newurl'); //zzz 속성을 가진 tag안에서 href속성을 가진 tag를 찾고 href의 '/' 값을 newurl속성값으로 변경
+//map.where('zzz').where('href').has(/ttt/).insert('newurl'); //위와 동일하지만 차이점은 has(정규식으로 수정대상을 검색)를 이용하여 href의 /ttt부분을 newurl속성값으로 변경
+//map.where('id').is('zzz').insert('newurl'); //id가 zzz인 곳에 newurl속성을 입력
+
+//map.class('test').to('test2');
+
+//var output = plates.bind(html2, data2, map);
+var output = plates.bind(html2, collection); //collection을 이용하여 반복적으로 html을 조작하도록 처리
+
+/*
+ * readFile과 readFileSync의 차이
+ * readFile은 비동기적으로 작동하고 보통 3번째 인자에 callback함수를 저장하낟.
+ * readFileSync는 동기적으로 작동한다.
+ */
 function showAuthor() {
 	logger.info('showAuthor');
-	this.res.writeHead(200, {'Content-Type' : 'text/plain'});
-	this.res.end('Carl Sagan');
+	//this.res.writeHead(200, {'Content-Type' : 'text/plain'});
+	//this.res.end('Carl Sagan');
+	//this.res.end(output);
+	
+	var ttt = this.res;
+	var html = fs.readFileSync('aaaa.html', 'utf-8');
+	//logger.info(html);
+	this.res.end(html);
 }
 
 function addInfo() {
@@ -29,7 +85,7 @@ function addInfo() {
 
 /*
  * express와 달리 route변수를 설정하여 url에 해당하는 동작을 지정할 수 있다.
- * {} 안의 옵션은 get,post,on(웹에서 사용하는건 아니고 CLI환경에서 사용)
+ * {} 안의 옵션은 get,post,on(웹에서 사용하는건 아니고 CLI환경에서 사용)등이 있다
  * '/aaa' : { get : aaa} -> /aaa url을 입력하면 aaa 함수를 실행한다.
  * 
  * '/aaa' : {
